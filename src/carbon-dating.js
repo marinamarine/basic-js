c9d1123da778919c76e9da027590665ce934f2c5
marinamarine <marinakarpovskaya@gmail.com>
2020-03-19 22:18:14 +0300

    feat: implement dateSample function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,11 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const RATE_CONSTANT = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let num = Number.parseFloat(sampleActivity);
+  if ((typeof sampleActivity == 'string') && (num > 0 && num <= 15)) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / num) / (RATE_CONSTANT / HALF_LIFE_PERIOD));
+  }
+  return false;
 };
