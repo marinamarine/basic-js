8f36ca10dd62d70aae975f71b0d3095fbadcd0a8
marinamarine <marinakarpovskaya@gmail.com>
2020-03-20 00:11:13 +0300

    feat: implement transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,34 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!(arr instanceof Array)) {
+        throw new Error;
+    }
+    let arrResult = [];
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next':
+                if (typeof arr[i + 1] !== "undefined") {
+                    i++;
+                }
+                break;
+            case "--discard-prev":
+                if (typeof arr[i - 1] !== "undefined") {
+                    arrResult.pop();
+                }
+                break;
+            case '--double-next':
+                if (typeof arr[i + 1] !== "undefined") {
+                    arrResult.push(arr[i + 1]);
+                }
+                break;
+            case '--double-prev':
+                if (typeof arr[i - 1] !== "undefined") {
+                    arrResult.push(arr[i - 1]);
+                }
+                break;
+            default:
+                arrResult.push(arr[i]);
+                break;
+        }
+    }
+    return arrResult;
 };
