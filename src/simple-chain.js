a8a5eb5f804a30221be878a1356163a9645f7fe1
marinamarine <marinakarpovskaya@gmail.com>
2020-03-23 00:18:59 +0300

    feat: add Chain maker task solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,31 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(`( ${String(value)} )`);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position === 'number' &&
+      position > 0 &&
+      position <= this.chain.length) {
+      this.chain.splice(position - 1, 1);
+    } else {
+      this.chain = [];
+      throw new Error();
+    }
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let str = this.chain.join('~~');
+    this.chain = [];
+    return str;
   }
 };
 
